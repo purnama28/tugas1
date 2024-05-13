2acf7c9dcb5ffdf17196aeb272f8bf8bf189de7d
purnama28 <purnama6375@gmail.com>
2024-05-13 22:50:30 +0800

    migration

---
+++ database/migrations/2024_05_13_143218_dokter.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('dokter', function (Blueprint $table) {
+            $table->integerIncrements('id_dokter');
+            $table->string('nama_dokter');
+            $table->string('jabatan');
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('dokter');
+    }
+};
